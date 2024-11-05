f78ded261bcc4e5cd7b3d4443fd67cdd0b3b16c7
Louise Aldridge <louisealdridge88@gmail.com>
2024-11-05 15:01:43 +0000

    Creating and styling banner components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
+import { FaExclamationTriangle, FaInfoCircle, FaTimesCircle, FaCheckCircle } from 'react-icons/fa';
+
 import Badge from "./components/badges/Badge"
+import Banner from "./components/banners/Banner"
 
 function App() {
 
   const colors = ["grey", "red", "yellow", "green", "blue", "indigo", "purple", "pink"]
 
-  //Badge Component
-  //Choose a color and shape. Color goes into color and shape into variant.
 
-  const badgeElements = colors.map((color) => {
-    return (
-      <div>
-        <Badge
-          color={color}
-          variant={"pill"}
-        />
-        <Badge
-          color={color}
-          variant={"square"}
-        />
-      </div>
 
-    )
-  })
+
 
   return (
-    <div className="badge-components">
-      {badgeElements}
-    </div>
+    <div>
+      <Banner
+        status={"success"}>
+        <h3> <FaCheckCircle className={"icon"} />Congratulations</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+      </Banner>
 
+      <Banner
+        status={"warning"}>
+        <h3> <FaExclamationTriangle className={"icon"} /> Warning </h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+      </Banner>
+
+      <Banner
+        status={"error"}>
+        <h3> <FaTimesCircle className={"icon"} /> There is a problem with your application</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+      </Banner>
+
+      <Banner
+        status={"neutral"}>
+        <h3> <FaInfoCircle className={"icon"} /> Update available</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+      </Banner>
+    </div>
   )
+
 }
 
 export default App
+
+
+
+// //Badge Component
+// //Choose a color and shape. Color goes into color and shape into variant.
+
+// const badgeElements = colors.map((color) => {
+//   return (
+//     <div>
+//       <Badge
+//         color={color}
+//         variant={"pill"}
+//       />
+//       <Badge
+//         color={color}
+//         variant={"square"}
+//       />
+//     </div>
+
+//   )
+// })
+
+// return (
+//   <div className="badge-components">
+//     {badgeElements}
+//   </div>
+
+// )
