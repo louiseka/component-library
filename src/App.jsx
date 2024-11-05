4480c14af1a34631062e9755546bc2c0da197ac1
Louise Aldridge <louisealdridge88@gmail.com>
2024-11-05 10:46:37 +0000

    Notes added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
 
   const colors = ["grey", "red", "yellow", "green", "blue", "indigo", "purple", "pink"]
 
+  //Badge Component
+  //Choose a color and shape. Color goes into color and shape into variant.
+
   const badgeElements = colors.map((color) => {
     return (
       <div>
