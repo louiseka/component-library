89ef4ad697c3539884d62b09850f5509db35f5da
Louise Aldridge <louisealdridge88@gmail.com>
2024-11-07 11:35:36 +0000

    Moving card component to notes

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,7 @@ function App() {
 
   return (
     <div>
-      <Card Icon={FaCloudUploadAlt} iconBackground={"yellow"}>
-        <h3> Easy Deployment </h3>
-        <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
-      </Card>
+
 
     </div>
 
@@ -25,60 +22,67 @@ export default App
 
 
 
-// //Badge Component
-// //Choose a color and shape. Color goes into color and shape into variant.
-// const badgeElements = colors.map((color) => {
-//   return (
-//     <div>
-//       <Badge color={color}
-//         variant={"pill"}>{color}</Badge>
+  // //Badge Component
+  // //Choose a color and shape. Color goes into color and shape into variant.
+  // const badgeElements = colors.map((color) => {
+  //   return (
+  //     <div>
+  //       <Badge color={color}
+  //         variant={"pill"}>{color}</Badge>
 
 
-//       <Badge color={color}
-//         variant={"square"}>
-//         {color}
+  //       <Badge color={color}
+  //         variant={"square"}>
+  //         {color}
 
-//       </Badge>
-
-
-//     </div>
-
-//   )
-// })
-
-// return (
-//   <div className="badge-components">
-//     {badgeElements}
-//   </div>
-
-// )
+  //       </Badge>
 
 
+  //     </div>
 
-//Banner Components 
+  //   )
+  // })
 
-/* <div>
-<Banner
-  status={"success"}>
-  <h3> <FaCheckCircle className={"icon"} />Congratulations</h3>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
-</Banner>
+  // return (
+  //   <div className="badge-components">
+  //     {badgeElements}
+  //   </div>
 
-<Banner
-  status={"warning"}>
-  <h3> <FaExclamationTriangle className={"icon"} /> Warning </h3>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
-</Banner>
+  // )
 
-<Banner
-  status={"error"}>
-  <h3> <FaTimesCircle className={"icon"} /> There is a problem with your application</h3>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
-</Banner>
 
-<Banner
-  status={"neutral"}>
-  <h3> <FaInfoCircle className={"icon"} /> Update available</h3>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
-</Banner>
-</div> */
+
+  //Banner Components 
+
+  /* <div>
+  <Banner
+    status={"success"}>
+    <h3> <FaCheckCircle className={"icon"} />Congratulations</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+  </Banner>
+  
+  <Banner
+    status={"warning"}>
+    <h3> <FaExclamationTriangle className={"icon"} /> Warning </h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+  </Banner>
+  
+  <Banner
+    status={"error"}>
+    <h3> <FaTimesCircle className={"icon"} /> There is a problem with your application</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+  </Banner>
+  
+  <Banner
+    status={"neutral"}>
+    <h3> <FaInfoCircle className={"icon"} /> Update available</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</p>
+  </Banner>
+  </div> */
+
+  // Card Component
+
+//   < Card Icon = { FaCloudUploadAlt } iconBackground = { "yellow"} >
+// <h3> Easy Deployment </h3>
+// <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+// </Card >
